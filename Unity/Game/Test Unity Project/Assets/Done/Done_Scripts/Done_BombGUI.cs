873d64dd3be179e85b9ccd3838a06f3b73671d14
Kevin Hsieh <hsieh63@scarletmail.rutgers.edu>
2014-02-28 13:17:09 -0500

    Added a bomb button
    Not very pretty, but functional
    Last update before meeting

---
+++ Unity/Game/Test Unity Project/Assets/Done/Done_Scripts/Done_BombGUI.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Done_BombGUI : MonoBehaviour {
+
+	public GUIStyle bombButtonStyle;
+	private string bombButtonText = "Click to use bomb";
+
+	void OnGUI() {
+		int screenHeight = Screen.height;
+		int screenWidth = Screen.width;
+		Rect buttonPlacement = new Rect ((screenWidth * 5) / 6, (screenHeight * 5) / 6, 150f, 150f);
+		//Rect buttonPlacement = new Rect (100, 100, 150, 150);
+		GUIContent bombButtonContent = new GUIContent (bombButtonText);
+		//GUI.Button (buttonPlacement, bombButtonContent, bombButtonStyle);
+		if (GUI.Button (buttonPlacement, bombButtonContent)) {
+			//bombButtonText = "Height: " + Screen.height + ". Width: " + Screen.width;
+			GameObject[] enemyObjects = GameObject.FindGameObjectsWithTag("Enemy");
+			foreach (GameObject target in enemyObjects) {
+				GameObject.Destroy (target);
+			}
+		}
+	}
+}
